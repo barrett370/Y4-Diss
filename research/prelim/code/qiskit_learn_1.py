50494e1aa13f81f5142a12715bdb9176e4b21d2a
sam <barrett370@gmail.com>
2020-10-08 13:34:27 +0100

    misc

--- research/prelim/code/qiskit_learn_1.py
+++ research/prelim/code/qiskit_learn_1.py
@@ -1,9 +1,39 @@
-from qiskit import QuantumCircuit, execute, Aer
+import numpy as np
+from qiskit import BasicAer
 from qiskit.visualization import plot_histogram
+from qiskit.aqua import QuantumInstance
+from qiskit.aqua.algorithms import Grover
+from qiskit.aqua.components.oracles import LogicalExpressionOracle, TruthTableOracle
+
+
+input_3sat = '''
+c example DIMACS-CNF 3-SAT
+p cnf 3 5
+-1 -2 -3 0
+1 -2 3 0
+1 2 -3 0
+1 -2 -3 0
+-1 2 3 0
+'''
+
+oracle = LogicalExpressionOracle(input_3sat)
+
+grover = Grover(oracle)
+
+backend = BasicAer.get_backend('qasm_simulator')
+quantum_instance = QuantumInstance(backend, shots=1024)
+result = grover.run(quantum_instance)
+print(result['result'])
+
+plot_histogram(result['measurement']).savefig('hist1.png')
 
 
 
+from qiskit import IBMQ
+IBMQ.load_account()
+provider = IBMQ.get_provider(hub='ibm-q')
+backend = provider.get_backend('ibmq_santiago')
 
+from qiskit.compiler import transpile 
 
-
-
+grover_compiled = transpile(result['circuit'], backend=backend, optimization_level=3) 
