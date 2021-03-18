5a588abe3871ea841e7ea1330cdfa3c2c5bdfc8a
sam <barrett370@gmail.com>
2021-03-18 14:02:58 +0000

    fixed edge case intersection detection

--- code/julia/src/bezier.jl
+++ code/julia/src/bezier.jl
@@ -75,38 +75,30 @@ toRealArray = i -> [[float(p.x),float(p.y)] for p in i]
 
 function bezInt(B1::BezierCurve,B2::BezierCurve) :: Tuple{Bool,Tuple{BezierCurve,BezierCurve}}
     @debug "bezInt called"
-    if B2 |> toRealArray != zeros(B2 |> length)
+    if B2 |> toRealArray == zeros(B2 |> length)
         return (false,([],[]))
     end
     
     n = 7
     c = Channel(4^(n-1))
-    main = Threads.@spawn bezInt(B1,B2,1,n,c)
+    main = bezInt(B1,B2,1,n,c)
     @debug "spawned main process"
     false_count = 0
-    while isopen(c)
-        res = take!(c)
-        if res[1]
-            @debug "Found intersection $res"
-            return res
-        else
-            false_count = false_count + 1
-            if length(res) > 1
-                if res[2] == "non-candidate"
-                    @debug "returning due to non-candidacy, $B1, $B2"
-                    return (false,([],[]))
-                end
-           else 
-                if false_count == 4^(n-1)
-                    #println("no intersect detected, all false, $false_count")
-                    return (false,([],[]))
-                end
-            end
-            
-        end
-    end
+    #while isopen(c)
+    #    res = take!(c)
+    #    if res[1]
+    #        @debug "Found intersection $res"
+    #        return res
+    #    else
+    #        false_count = false_count + 1
+    #            if false_count == 4^(n-1)
+    #                println("no intersect detected, all false, $false_count")
+    #                return (false,([],[]))
+    #            end
+    #    end
+    #end
     #println("no intersect detected, c closed")
-    return false
+    return main
 end
 
 function deCasteljau(B::BezierCurve,t::Real)::Tuple{BezierCurve,BezierCurve}
@@ -116,22 +108,27 @@ end
 function bezInt(B1::BezierCurve, B2::BezierCurve, rdepth::Int,rdepth_max,ret_channel::Channel)
     if rdepth +1 > rdepth_max
         @debug "rdepth reached" 
-        put!(ret_channel,false)
+        #put!(ret_channel,false)
+        return (false,([],[]))
     end
     ε  = 2.5 # TODO tune param
     toLuxPoints = b -> map(p-> Luxor.Point(p[1],p[2]),b)
     if length(B1) < 2 || length(B2) < 2
         @show "error not enough control points"
-        put!(ret_channel,false)
+#        put!(ret_channel,false)
+        return (false,([],[]))
     else
         #if length(convex_hull(B1 |> toRealArray) ∪ convex_hull(B2 |> toRealArray)) != 0 # Union of the convex hulls of the control points is non-empty
         @debug "testing hulls"
         dupe_points = length((B1 |> toRealArray) ∩ (B2 |> toRealArray)) != 0
         if !dupe_points
             @debug "no dupe points"
+            ts = rand(0:0.05:1,50)
+            B1_points = map(t -> B1(t), ts)
+            B2_points = map(t -> B2(t), ts)
             hull_intersection = length(filter(each -> each == 1,
-                         [Luxor.isinside(p, B1 |> toRealArray |> convex_hull |> toLuxPoints) for p in B2 |> toRealArray |> convex_hull |> toLuxPoints] ∩
-                                          [Luxor.isinside(p, B2 |> toRealArray |> convex_hull |> toLuxPoints) for p in B1|> toRealArray |> convex_hull  |> toLuxPoints])) != 0
+                         [Luxor.isinside(p, B1 |> toRealArray |> convex_hull |> toLuxPoints) for p in B2_points |> toRealArray |> convex_hull |> toLuxPoints] ∪
+                                          [Luxor.isinside(p, B2 |> toRealArray |> convex_hull |> toLuxPoints) for p in B1_points |> toRealArray |> convex_hull  |> toLuxPoints])) != 0
         else
             @debug "setting intersection to default (true)"
             hull_intersection = true
@@ -143,29 +140,37 @@ function bezInt(B1::BezierCurve, B2::BezierCurve, rdepth::Int,rdepth_max,ret_cha
             @debug "B1 and B2 are a candidate pair"
             if diam(B1 ∪ B2) < ε
                 #@show rdepth
-                put!(ret_channel,(true,(B1,B2)))
+                #put!(ret_channel,(true,(B1,B2)))
+                return (true,(B1,B2))
             else # subdivides the curve with the larger diameter
     #            println("subdividing")
+                tasks::Array{Task} = []
                 if diam(B1) >= diam(B2)
                     (B1_1,B1_2) = deCasteljau(B1,0.5)
-                    Threads.@spawn bezInt(B1_1,B2,rdepth+1,rdepth_max, ret_channel)
-                    Threads.@spawn bezInt(B1_2,B2,rdepth+1,rdepth_max, ret_channel)
+                    append!(tasks,[Threads.@spawn bezInt(B1_1,B2,rdepth+1,rdepth_max, ret_channel)])
+                    append!(tasks,[Threads.@spawn bezInt(B1_2,B2,rdepth+1,rdepth_max, ret_channel)])
                 else
                     (B2_1,B2_2) = deCasteljau(B2,0.5)
-                    Threads.@spawn bezInt(B1,B2_1,rdepth+1,rdepth_max, ret_channel)
-                    Threads.@spawn bezInt(B1,B2_2,rdepth+1,rdepth_max, ret_channel)
+                    append!(tasks,[Threads.@spawn bezInt(B1,B2_1,rdepth+1,rdepth_max, ret_channel)])
+                    append!(tasks,[Threads.@spawn bezInt(B1,B2_2,rdepth+1,rdepth_max, ret_channel)])
                 end
-                #@show "created tasks"
-
+                @debug "created tasks"
+                for task in tasks
+                    res = fetch(task)
+                    if res[1]
+                        return res
+                    end
+                end
+                return (false,([],[]))
 
             end
         else
             @debug "B1 and B2 are not candidates therefore, cannot intersect."
             #@show "initial individuals are not candidates"
-            put!(ret_channel,(false,"non-candidate"))
+            #put!(ret_channel,(false,"non-candidate"))
+            return (false,([],[]))
         end
     end
-    return
 end
 
 
