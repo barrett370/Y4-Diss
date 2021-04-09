a52882261ed77ac26397c26655850947be5fe249
sam <barrett370@gmail.com>
2021-04-09 17:40:03 +0100

    EOD

--- code/julia/src/demoRoadNetwork.jl
+++ code/julia/src/demoRoadNetwork.jl
@@ -1,91 +1,91 @@
 include("roadNetwork.jl")
 import GraphPlot
 
-b1(x) = 0
-b2(x) = 12
+road13_b1(x) = 0
+road13_b2(x) = 12
 road13 = Road(
-    b1,
-    b2,
+    road13_b1,
+    road13_b2,
     [],
     10
 )
-b1(x) = 0
-b2(x) = 15
+road31_b1(x) = 0
+road31_b2(x) = 15
 road31 =Road(
-    b1,
-    b2,
+    road31_b1,
+    road31_b2,
     [],
     10
 )
 
-b1(x) = 0
-b2(x) = 12
+road32_b1(x) = 0
+road32_b2(x) = 12
 road32 = Road(
-    b1,
-    b2,
+    road32_b1,
+    road32_b2,
     [],
     14
 )
 
-b1(x) = 0
-b2(x) = 12
+road23_b1(x) = 0
+road23_b2(x) = 12
 road23 = Road(
-    b1,
-    b2,
+    road23_b1,
+    road23_b2,
     [Rectangle(2,7,Point(4,1))],
     14
 )
 
-b1(x) = 0
-b2(x) = 5
+road43_b1(x) = 0
+road43_b2(x) = 5
 road43 = Road(
-    b1,
-    b2,
+    road43_b1,
+    road43_b2,
     [],
     12
 )
 
-b1(x) = 0
-b2(x) = 15
+road24_b1(x) = 0
+road24_b2(x) = 15
 road24 = Road(
-    b1,
-    b2,
+    road24_b1,
+    road24_b2,
     [],
     20
 )
 
-b1(x) = 0
-b2(x) = 12
+road25_b1(x) = 0
+road25_b2(x) = 12
 road25 = Road(
-    b1,
-    b2,
+    road25_b1,
+    road25_b2,
     [],
     10
 )
 
-b1(x) = 0
-b2(x) = 12
+road52_b1(x) = 0
+road52_b2(x) = 12
 road52 = Road(
-    b1,
-    b2,
+    road52_b1,
+    road52_b2,
     [],
     10
 )
 
-b1(x) = 0
-b2(x) = 12
+road45_b1(x) = 0
+road45_b2(x) = 12
 road45 = Road(
-    b1,
-    b2,
+    road45_b1,
+    road45_b2,
     [],
     7
 )
 
-b1(x) = 0
-b2(x) = 12
+road54_b1(x) = 0
+road54_b2(x) = 12
 road54 = Road(
-    b1,
-    b2,
+    road54_b1,
+    road54_b2,
     [],
     7
 )
